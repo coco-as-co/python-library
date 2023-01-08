8c48ea9a4b331bd56c0625ed5518ded06bc05c03
Arkeonn <tgeoffron@myges.fr>
2023-01-08 17:24:53 +0100

    feat: ✨ First draft for libraries CRUD

--- library/forms.py
+++ library/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from .models import Library
 
 class SignUpForm(UserCreationForm):
     username = forms.CharField(max_length=30, required=True)
@@ -11,3 +12,12 @@ class SignUpForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['username', 'email', 'first_name', 'last_name', 'password1', 'password2']
+
+class LibraryForm(forms.ModelForm):
+    name = forms.CharField(max_length=80)
+    city = forms.CharField(max_length=80)
+    address = forms.CharField(max_length=80)
+
+    class Meta:
+        model = Library
+        fields = ['name', 'city', 'address']
