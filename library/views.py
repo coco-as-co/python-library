8a43609f7c5e65c6fb954b4dbc84880c66f927d6
Océane <oceane.renoux@hotmail.fr>
2023-01-15 18:34:59 +0100

    [ADD] Voir que ces livres dans la liste /book

--- library/views.py
+++ library/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse, HttpResponseNotFound
 from django.contrib import messages
-from .forms import SignUpForm , BookForm , Book
-from .models import Library
+from .forms import SignUpForm , BookForm , Book , Library
+
 
 
 def index(request):
@@ -28,27 +28,31 @@ def home(request):
     return HttpResponseNotFound('<h1>Page not found</h1>')
 
 def book(request):
-    if request.user.is_authenticated:
-        books = Book.objects.all()
+    if request.user.is_authenticated :
+        libraryList = Library.objects.filter(owner=request.user.id)
+        if(len(libraryList) == 0):
+             books = []
+        else:
+            books = Book.objects.filter(library = libraryList[0].id)
+            print(books)
         return render(request, 'book/book.html', {'books': books})
     return HttpResponseNotFound('<h1>Page not found</h1>')
 
 def addBook(request):
     if request.user.is_authenticated:
-        print('bla',request.user.id )
-        if request.method == 'POST':
-            
-            form = BookForm( request.POST,request.FILES,  )
-            if form.is_valid():
-                form.save()
-                messages.success(request, 'Book added successfully')
+            if request.method == 'POST':
+                
+                form = BookForm( request.POST,request.FILES,userId = request.user.id  )
+                if form.is_valid():
+                    form.save()
+                    messages.success(request, 'Book added successfully')
 
-                return redirect('book')
-        else:
-            form = BookForm(userId = request.user.id)
-        
-        context = {'form': form}
-        return render(request, 'book/addBook.html',context)
+                    return redirect('book')
+            else:
+                form = BookForm(userId = request.user.id)
+            
+            context = {'form': form}
+            return render(request, 'book/addBook.html',context)
     return HttpResponseNotFound('<h1>Page not found</h1>')
 
 def editBook(request, id):
