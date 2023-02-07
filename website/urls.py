6e66e84cccb4a44860bc3921a42d09ab46a1bf3b
Arkeonn <tgeoffron@myges.fr>
2023-02-07 13:58:34 +0100

    fix: Implementing landing page

--- website/urls.py
+++ website/urls.py
@@ -26,6 +26,7 @@ urlpatterns = [
     path('register/', views.register, name='register'),
     path('login/', auth_views.LoginView.as_view(template_name='registration/login.html', redirect_authenticated_user=True, form_class=forms.LoginForm), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='registration/logout.html'), name='logout'),
+    path('', views.index, name='index'),
 ]
 
 if settings.DEBUG:
